a5a0a79258915545d85fe958894c8884ab572df7
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-21 10:28:23 +0300

    1D Array

--- Demo session 5/Program.cs
+++ Demo session 5/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Array
+            #region 1D Array
+            //int[] numbers01 = new int[3];
+            //int[] numbers02 = new int[] { 1, 2, 3 };
+            //int[] numbers03 = { 1, 2, 3 };
+            //int[] numbers04 = [1, 2, 3];
+            #endregion
+            #endregion
         }
     }
 }
