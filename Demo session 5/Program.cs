f18f67d25d2f19a283b4f5f73f73cb2e02c8c6e5
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-21 11:32:26 +0300

    Array methods

--- Demo session 5/Program.cs
+++ Demo session 5/Program.cs
@@ -68,6 +68,35 @@
             //JaggedArray[2] = [30, 40, 50];
             #endregion
             #endregion
+            #region Array methods
+            #region Class member methods
+            //int[] numbers = { 5, 3, 4, 1, 2, 4 };
+            //Array.Sort(numbers);
+            //Array.Reverse(numbers);
+            //Array.Clear(numbers,2,3);
+            //Console.WriteLine(Array.IndexOf(numbers,4));
+            //Console.WriteLine(Array.LastIndexOf(numbers,4));
+            //Array.Resize(ref numbers,10);
+            //foreach (int item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //int[] arr01 = { 1, 2, 3, 4, 5 };
+            //int[] arr02 = new int[4];
+            //Array.Copy(arr01,arr02,4);
+            //Array.ConstrainedCopy(arr01, 1, arr02, 1, 2);
+            //Console.ReadKey();
+            #endregion
+            #region Object member methods
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //numbers.GetLength(0);
+            //Console.WriteLine(numbers.Length);
+            //int element = (int) numbers.GetValue(1);
+            //element = numbers[1];
+            //numbers[0] = 100;
+            //numbers.SetValue(100, 0);
+            #endregion
+            #endregion
         }
     }
 }
