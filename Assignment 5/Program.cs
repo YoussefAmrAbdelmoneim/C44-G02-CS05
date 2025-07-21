922c8dbdb95635831eda12e2fc511cbfbcec2eab
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-21 13:43:23 +0300

    Array Q1

--- Assignment 5/Program.cs
+++ Assignment 5/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Array Q1
+            //int sum = 0;
+            //int size;
+            //Console.WriteLine("Please enter size of array");
+            //int.TryParse(Console.ReadLine(), out size);
+            //int[] numbers = new int[size];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write("enter number in array : ");
+            //    int.TryParse(Console.ReadLine(), out numbers[i]);
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine($"Sum of numbers : {sum}");
+            #endregion
         }
     }
 }
